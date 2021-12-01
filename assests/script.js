17a1bbb43aeda808069096c0c781a92c60e965ad
ckitazumi <codykitazumi@gmail.com>
2021-12-01 03:35:31 -0800

    updated script tag in index

--- assests/script.js
+++ assests/script.js
@@ -7,6 +7,7 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var special = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", ", ",", "]", "^", "_", "`", "{", "|", "}", "~"]
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
